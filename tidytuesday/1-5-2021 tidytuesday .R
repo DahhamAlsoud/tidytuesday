7569d0cf2ca937121408f184f69aae4622680ca5
Julia Catalano <76126692+juliacat23@users.noreply.github.com>
2021-01-05 19:35:26 -0500

    Update 1-5-2020 tidytuesday.R

--- tidytuesday/1-5-2021 tidytuesday .R
+++ tidytuesday/1-5-2021 tidytuesday .R
@@ -1,7 +1,11 @@
 library(tidyverse)
 library(tidytuesdayR)
 library(countrycode)
+library(ggmap)
+library(RColorBrewer)
 library(bbplot)
+library(ggthemes)
+library(ggrepel)
 
 #Read in week 2 data 
 tt_data <- tt_load(2021, week=2)
@@ -32,11 +36,36 @@ transit_cost$continent[is.na(transit_cost$continent)] <- "Europe"
 #     Start Year 
 #     Cost/Km in Millions of USD 
 
-
 transit_plot <- transit_cost %>% 
-  select(continent, country, city,length, start_year, cost_km_millions) %>%
-  ggplot(aes(x = continent, y = cost_km_millions)) + 
-  geom_boxplot()
-  
+  select(continent, country, city, start_year, cost_km_millions, line) %>% 
+  filter(start_year <= 2020) %>% 
+  ggplot(aes(x = start_year, y = cost_km_millions, color = continent)) + 
+  geom_point(position = "jitter") + 
+  geom_text_repel(
+    data = filter(transit_cost, line == "East Side Access"),
+    aes(label = city),
+    force = 1, point.padding = 0.1,
+    segment.size = 0.2) +
+  geom_text_repel(
+    data = filter(transit_cost, line == "Second Avenue Phase 2"),
+    aes(label = city),
+    force = 1, point.padding = 0.1,
+    segment.size = 0.2) +
+  geom_text_repel(
+    data = filter(transit_cost, line == "Circle Line Stage 6"),
+    aes(label = city),
+    force = 1, point.padding = 0.1,
+    segment.size = 0.2) +
+  geom_text_repel(
+    data = filter(transit_cost, line == "City Rail Link"),
+    aes(label = city),
+    force = 1, point.padding = 0.1,
+    segment.size = 0.2) +
+  theme_fivethirtyeight() + 
+  labs(x = "Project Start Year", y = "Cost/KM in Millions of USD", 
+       title = "Transient Costs Have Slightly Increased Since 1982", 
+       subtitle = "A #tidytuesday exercise", 
+       caption = "Data: Transit Costs Project | Plot: @juliacat23")
+ 
 
 transit_plot
