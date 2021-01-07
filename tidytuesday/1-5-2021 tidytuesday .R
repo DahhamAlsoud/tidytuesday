f1fd4cd0611f1335c0a4a293f72dc77419397776
Julia Catalano <76126692+juliacat23@users.noreply.github.com>
2021-01-07 11:45:17 -0500

    update 1-5-2021 tidytuesday.R

--- tidytuesday/1-5-2021 tidytuesday .R
+++ tidytuesday/1-5-2021 tidytuesday .R
@@ -36,36 +36,65 @@ transit_cost$continent[is.na(transit_cost$continent)] <- "Europe"
 #     Start Year 
 #     Cost/Km in Millions of USD 
 
+#load 538 theme 
+theme_538 <- function(base_size = 12, base_family = "Gill Sans") {
+  
+  theme_grey(base_size = base_size, base_family = base_family) %+replace%
+    theme(
+      panel.grid.minor = element_blank(),
+      axis.ticks = element_blank(),
+      text = element_text(family = "Gill Sans", size = base_size),
+      axis.text = element_text(face = "plain", color = "black", size = base_size),
+      axis.title = element_text(face = "bold", size = rel(1.33)),
+      axis.title.x = element_text(margin = margin(0.5, 0, 0, 0, unit = "cm")),
+      axis.title.y = element_text(margin = margin(0, 0.5, 0, 0, unit = "cm"), angle =90),
+      plot.title = element_text(face = "bold", size = rel(1.67), hjust = 0),
+      plot.title.position = "plot",
+      plot.subtitle = element_text(size = 16, margin = margin(0.2, 0, 1, 0, unit = "cm"), hjust = 0),
+      plot.caption = element_text(size = 11, margin = margin(1, 0, 0, 0, unit = "cm"), hjust = 1),
+      plot.background = element_rect(fill = "#f0f0f0", color = NA),
+      panel.background = element_rect(fill = "#f0f0f0", color = NA),
+      panel.grid.major =  element_line(color = "#d0d0d0"),
+      panel.border = element_blank(),
+      plot.margin = margin(0.5, 1, 0.5, 1, unit = "cm"),
+      strip.background = element_blank(),
+      strip.text = element_text(size = rel(1.33), face = "bold"))}
+  
 transit_plot <- transit_cost %>% 
   select(continent, country, city, start_year, cost_km_millions, line) %>% 
   filter(start_year <= 2020) %>% 
   ggplot(aes(x = start_year, y = cost_km_millions, color = continent)) + 
   geom_point(position = "jitter") + 
   geom_text_repel(
-    data = filter(transit_cost, line == "East Side Access"),
+    data = filter(transit_cost, e == "7411"),
     aes(label = city),
     force = 1, point.padding = 0.1,
     segment.size = 0.2) +
   geom_text_repel(
-    data = filter(transit_cost, line == "Second Avenue Phase 2"),
+    data = filter(transit_cost, e == "8210"),
     aes(label = city),
     force = 1, point.padding = 0.1,
     segment.size = 0.2) +
   geom_text_repel(
-    data = filter(transit_cost, line == "Circle Line Stage 6"),
+    data = filter(transit_cost, e == "7329"),
     aes(label = city),
     force = 1, point.padding = 0.1,
     segment.size = 0.2) +
   geom_text_repel(
-    data = filter(transit_cost, line == "City Rail Link"),
+    data = filter(transit_cost, e == "7435"),
     aes(label = city),
     force = 1, point.padding = 0.1,
     segment.size = 0.2) +
-  theme_fivethirtyeight() + 
+  geom_text_repel(
+    data = filter(transit_cost, e == "7417"),
+    aes(label = city),
+    force = 1, point.padding = 0.1,
+    segment.size = 0.2) +
+  theme_538() + 
   labs(x = "Project Start Year", y = "Cost/KM in Millions of USD", 
        title = "Transient Costs Have Slightly Increased Since 1982", 
-       subtitle = "A #tidytuesday exercise", 
-       caption = "Data: Transit Costs Project | Plot: @juliacat23")
+       subtitle = "The top 5 most expensive projects were in the Americas", 
+       caption = "Data: Transit Costs Project | Plot: @juliacat23 | a #TidyTuesday exercise")
  
 
 transit_plot
